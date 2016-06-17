fa9113dfbfb06c8bf09d52468e9a38d9365e3348
DesaiV <vishal.n.desai@gmail.com>
2016-06-16 23:19:03 -0700

    Task-update-0-3
    Made changes for all test cases

--- src/test/java/com/secondLargestNumber/test/SecondLargestNumber/TestScondLargestNumber.java
+++ src/test/java/com/secondLargestNumber/test/SecondLargestNumber/TestScondLargestNumber.java
@@ -4,31 +4,65 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestScondLargestNumber {
-	
-	//int[] a= {1,4,3,2,5,6,8,0,9};
-		SecondLargestNumber sn = new SecondLargestNumber();
-		
-		@Test
-		public void testArrayNotNull(){
-			int[] arry={};
-			sn.arrayLength(arry);
-			Assert.assertNotNull(sn);
-		}
-		
-		@Test
-		public void testArrayOnlyOneElement(){
-			int[] arry={1};
-			sn.arrayLength(arry);
-			Assert.assertNotEquals(null, sn);
-		}
-		
-		@Test
-		public void testScondLargestNumber(){
-			int[] arry={0,5,7,8,8,9,9,9,9};
-			int b=sn.SecondLargestNumber(arry);
-			System.out.println(b);
-			Assert.assertEquals(b, 8);
-		}	
 
+	SecondLargestNumber sn = new SecondLargestNumber();
+
+	/**
+	 * Test Case 1: Check array is not null
+	 */
+	@Test
+	public void testArrayNotNull() {
+		int[] arry = null;
+		Assert.assertNull(sn.unsortedArray(arry), "Array is empty");
+
+	}
+
+	/**
+	 * Test Case 2: Check array has only one element
+	 */
+	@Test
+	public void testArrayOnlyOneElement() {
+		int[] arry = { 1 };
+		Assert.assertNull(sn.unsortedArray(null), "Array has only one elements");
+	}
+
+	/**
+	 * Test Case 3: Check second largest number with duplicate array element
+	 */
+	@Test
+	public void testScondLargestNumber() {
+		int[] arry = { 0, 5, 7, 8, 8, 9, 9, 9, 9 };
+		int b = sn.unsortedArray(arry);
+		Assert.assertEquals(b, 8);
+	}
+
+	/**
+	 * Test Case 4: Check second largest number when all numbers are different
+	 */
+	@Test
+	public void withoutsorting() {
+		int[] arry = { 1, 4, 7, 3, 0, 9, 3, 6 };
+		int b = sn.unsortedArray(arry);
+		Assert.assertEquals(b, 7);
+	}
+
+	/**
+	 * Test Case 5: Check all array numbers are same
+	 */
+	@Test
+	public void allElementsAreSame() {
+		int[] arry = { 4, 4, 4, 1 };
+		Assert.assertNull(sn.unsortedArray(arry), "All elements are same");
+	}
+
+	/**
+	 * Test Case 6: Check second largest number if array with two numbers
+	 */
+	@Test
+	public void testArrayWithTwoElements() {
+		int[] arry = { 1, 2 };
+		int b = sn.unsortedArray(arry);
+		Assert.assertEquals(b, 1);
+	}
 
 }
